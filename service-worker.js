7d12d18afcac6fa5f39fdfad8936bee4a0959382
avgusta92 <avgusta92@gmail.com>
2021-05-25 11:32:55 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.35369277eabbfb9f41d8e1c2d600fc7c.js"
+  "./precache-manifest.60f6f8739e8836c0bb1c87e3c9451b24.js"
 );
 
 self.addEventListener('message', (event) => {
